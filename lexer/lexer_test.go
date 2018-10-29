bc640a44afc531c9b693577d013e336631115747
Rand01ph <361212419@qq.ccom>
2018-10-30 00:26:28 +0800

    finish 1.3

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,13 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -57,12 +63,12 @@ func TestNextToken(t *testing.T) {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Errorf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
-			t.Errorf("tests[%d] - literal wrong. expected=%q, got=%q",
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
